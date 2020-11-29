cff41e5d6388e9e40af87e2bcc0a2cd960c26226
Adrian Vinas <adrianvinasrosa@gmail.com>
2020-11-29 12:43:22 -0400

    score in localstorage

--- js/score.js
+++ js/score.js
@@ -2,18 +2,24 @@ export default class Score {
     constructor({ startPoints }) {
         this.points = startPoints
         this.$scoreContainer = document.querySelector('#score-container')
+        this.scoreStorage(this.points)
         this.render()
     }
     buildScore() {
         return `
-            <h2 class="text-3xl font-semibold mt-8">Score: ${this.points}</h2>
+            <h2 class="text-3xl font-semibold mt-8">Score: ${localStorage.getItem("points")}</h2>
         `
     }
     upScore() {
         this.points++
+        this.scoreStorage(this.points)
     }
     cleanScore() {
         this.points = 0
+        this.scoreStorage(this.points)
+    }
+    scoreStorage(points) {
+        localStorage.setItem("points", points)
     }
     render() {
         this.$scoreContainer.innerHTML = this.buildScore()
